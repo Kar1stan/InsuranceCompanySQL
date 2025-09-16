44fbc03cfa2b98a1dcf13c2c990144f8cc2b77e8
Karastan Vladislav <90850632+Kar1stan@users.noreply.github.com>
2025-09-16 09:20:40 +0300

    Update EmailNotificationObserver.java

--- src/main/java/com/solvd/project/model/observer/EmailNotificationObserver.java
+++ src/main/java/com/solvd/project/model/observer/EmailNotificationObserver.java
@@ -5,7 +5,6 @@ import com.solvd.project.model.Claims;
 public class EmailNotificationObserver implements ClaimObserver {
     @Override
     public void onClaimUpdated(Claims claim) {
-        // Example: send email notification
         System.out.println("Email sent for claim update: " + claim.getClaimId());
     }
 }
